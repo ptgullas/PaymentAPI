83a3f3467d3f3c9233978f60114e270d8d19e0b8
ptgullas <4305689+ptgullas@users.noreply.github.com>
2021-05-29 17:36:36 -0400

    flesh out service

--- PaymentApp/src/app/shared/payment-detail.service.ts
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {PaymentDetail} from './payment-detail.model';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
+  formData: PaymentDetail = new PaymentDetail();
+  readonly baseURL = 'http://localhost:612316/api/PaymentDetail';
+  list: PaymentDetail[];
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  postPaymentDetail() {
+    return this.http.post(this.baseURL, this.formData);
+  }
+  putPaymentDetail() {
+    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`, this.formData);
+  }
+  deletePaymentDetail(id: number) {
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
+
+  refreshList() {
+    this.http.get(this.baseURL)
+    .toPromise()
+    .then(res => this.list = res as PaymentDetail[]);
+  }
 }
